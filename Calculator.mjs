ccba457672bc50175da058c6a361330c25c3ae48
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-04-19 14:04:08 -0400

    Restart functionality

--- Calculator.mjs
+++ Calculator.mjs
@@ -1,55 +1,6 @@
 const Calculator = () => {
-    const input = []
-    const operands = []
-    const operators = []
 
-    const pushInput = (value) => {
-        input.push(value)
-        console.log(input)
-    }
-
-    const getInput = () => input
-
-    const allClear = () => {
-        input.length = 0
-        console.log(input)
-
-    }
-
-    const del = () => {
-        console.log(input.length)
-        const operators = ['(', ')', '/', '-', '+']
-        if(operators.includes(input[input.length - 1])) {
-            input.pop()
-        }
-        else{
-            let number = input[input.length - 1]
-            if(number && number.length > 1){
-                console.log('test1')
-
-                number = number.substring(0 , number.length - 1)
-                input.pop()
-                input.push(number)
-            }
-            else if(input.length === 1) {
-                console.log('test2')
-                input.pop()
-            }
-            else{
-                console.log('test3')
-
-                input.pop()
-            }
-
-        }
-        console.log(input)
-        
-
-    }
-
-    
-
-    return { pushInput, allClear, del, getInput }
+    return { }
 }
 
 export { Calculator }
--- UI.mjs
+++ UI.mjs
@@ -1,59 +1,13 @@
 import { Calculator } from "./Calculator.mjs"
 
 const UI = () => {
-    const calculator = Calculator()
-    const arithmetic = document.querySelector('.arithmetic')
-
-    const del = () => {
-        arithmetic.textContent = arithmetic.textContent.substring(0, arithmetic.textContent.length - 1)
-    }
-
-    const allClear = () => {
-        arithmetic.textContent = ''
-    }
-
+ 
     const buttonListener = () => {
         const buttons = document.querySelectorAll('button')
-        let operand = ''
-        let operator
-        
         const answer = document.querySelector('.answer')
+        
         buttons.forEach(button => {
             button.addEventListener('click', () => {
-                if(button.classList.contains('operand')){
-                    operand += button.id
-                    arithmetic.textContent += button.id
-                    
-                } 
-                else if(button.classList.contains('operator') &&
-                        button.id !== '()'){
-                    if(arithmetic.textContent){
-                        arithmetic.textContent += button.id
-                        calculator.pushInput(operand)
-                        calculator.pushInput(button.id)
-                        operand = ''
-                    }
-                    else {
-                        answer.textContent = 'Invalid format used'
-                    }
-
-
-                }
-                else if(button.id === 'ac'){
-                    allClear()
-                    calculator.allClear()
-                    operand = ''
-
-                }
-                else if(button.id === 'del'){
-                    del()
-                    console.log(arithmetic.textContent)
-                    if(arithmetic.textContent !== '') calculator.del()
-                    operand = ''
-                }
-                else if(button.id === '()'){
-                    console.log(arithmetic.textContent)
-                }
         
             })
         })
